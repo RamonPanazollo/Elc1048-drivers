6da4d572eaf89c3c90087d19d336395d737584a6
RamonPanazollo <105501154+RamonPanazollo@users.noreply.github.com>
2022-06-07 17:33:19 -0300

    polling

--- GccBoardProject2/GccBoardProject2/src/main.c
+++ GccBoardProject2/GccBoardProject2/src/main.c
@@ -33,53 +33,32 @@
  */
 #include <asf.h>
 
-void configure_extint_channel(void)
-{
-	struct extint_chan_conf config_extint_chan;
-	extint_chan_get_config_defaults(&config_extint_chan);
-	config_extint_chan.gpio_pin = BUTTON_0_EIC_PIN;
-	config_extint_chan.gpio_pin_mux = BUTTON_0_EIC_MUX;
-	config_extint_chan.gpio_pin_pull = EXTINT_PULL_UP;
-	config_extint_chan.detection_criteria = EXTINT_DETECT_BOTH;
-	extint_chan_set_config(BUTTON_0_EIC_LINE, &config_extint_chan);
-}
+void configure_extint_channel(void);
 
-void extint_detection_callback(void)
-{
-	bool pin_state = port_pin_get_input_level(BUTTON_0_PIN); 
-	port_pin_set_output_level(LED_0_PIN, pin_state);
-}void configure_extint_callbacks(void)
-{
-	extint_register_callback(extint_detection_callback,
-	BUTTON_0_EIC_LINE,
-	EXTINT_CALLBACK_TYPE_DETECT);
-	extint_chan_enable_callback(BUTTON_0_EIC_LINE,
-	EXTINT_CALLBACK_TYPE_DETECT);
-}
-
 int main (void)
 {
-	
 	system_init();
-	extint_detection_callback();
 	configure_extint_channel();
-	configure_extint_callbacks();
-	system_interrupt_enable_global();	
+	system_interrupt_enable_global();
+	
+	//POLLING
 	while (true) {
-		/* Do nothing - EXTINT will fire callback asynchronously */
-	}
+	 if (extint_chan_is_detected(BUTTON_0_EIC_LINE)) {
+		// Do something in response to EXTINT edge detection
+		bool button_pin_state = port_pin_get_input_level(BUTTON_0_PIN);
+		port_pin_set_output_level(LED_0_PIN, button_pin_state);
+		extint_chan_clear_detected(BUTTON_0_EIC_LINE);
+		}
+	}
+}
 
-	/* Insert application code here, after the board has been initialized. */
-
-	/* This skeleton code simply sets the LED to the state of the button. */
-	//while (1) {
-		/* Is button pressed? */
-		//if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
-			/* Yes, so turn LED on. */
-			//port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
-		//} else {
-			/* No, so turn LED off. */
-			//port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
-		//}
-	//}
+void configure_extint_channel(void)
+{
+ struct extint_chan_conf config_extint_chan;
+ extint_chan_get_config_defaults(&config_extint_chan);
+ config_extint_chan.gpio_pin = BUTTON_0_EIC_PIN;
+ config_extint_chan.gpio_pin_mux = BUTTON_0_EIC_MUX;
+ config_extint_chan.gpio_pin_pull = EXTINT_PULL_UP;
+ config_extint_chan.detection_criteria = EXTINT_DETECT_BOTH;
+ extint_chan_set_config(BUTTON_0_EIC_LINE, &config_extint_chan);
 }
